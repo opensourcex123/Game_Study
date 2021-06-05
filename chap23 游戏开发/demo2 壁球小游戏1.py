e529841d2e4f04b7c838d4c6b7ccca51176d5bfd
nfx_git <13561592791@163.com>
2021-06-05 21:37:57 +0800

    键盘事件和屏幕刷新率

--- chap23 游戏开发/demo2 壁球小游戏1.py
+++ chap23 游戏开发/demo2 壁球小游戏1.py
@@ -3,18 +3,29 @@
 import pygame,sys
 
 pygame.init()
-size=width,height=2000,2000
+size=width,height=600,400
 speed=[1,1]
 BLACK=0,0,0
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption('老必登球')
-ball=pygame.image.load('国际巨星.png')
+ball=pygame.image.load('PYG02-ball.gif')
 ballrect=ball.get_rect()
+fps=300 #设置小球的运动速度，实际设置循环的频率
+fclock=pygame.time.Clock()
 
 while True:
    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                speed[0]=speed[0] if speed[0]==0 else (abs(speed[0])-1)*int(speed[0]/abs(speed[0]))
+            elif event.key == pygame.K_RIGHT:
+                speed[0]=speed[0]+1 if speed[0]>=0 else speed[0]-1
+            elif event.key == pygame.K_UP:
+                speed[1] = speed[1] + 1 if speed[1] >= 0 else speed[1] - 1
+            elif event.key == pygame.K_DOWN:
+                speed[1]=speed[1] if speed[1]==0 else (abs(speed[1])-1)*int(speed[1]/abs(speed[1]))
    ballrect=ballrect.move(speed[0],speed[1])
    if ballrect.left<0 or ballrect.right>width:
        speed[0]=-speed[0]
@@ -23,3 +34,4 @@ while True:
    screen.fill(BLACK)
    screen.blit(ball,ballrect)
    pygame.display.update()
+   fclock.tick(fps) #控制帧的刷新速度
